f579860797565049001d29e755a3d245b5812a18
mgstabrani <mgstabraniitb@gmail.com>
2020-08-20 16:03:25 +0700

    First commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>SMAN Sumatera Selatan</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container mt-5 ">
+            <div class="row">
+                <div class="card mb-2 mr-2" style="width: 18rem;">
+                    <img src="dolphin.png" class="card-img-top" alt="dolphin">
+                    <div class="card-body">
+                        <h3 class="card-text">Dolphin</h3>
+                    </div>
+                </div>
+                <div class="card mb-2 mr-2" style="width: 18rem;">
+                    <img src="img/dove.png" class="card-img-top" alt="dove">
+                    <div class="card-body">
+                        <h3 class="card-text">Dove</h3>
+                    </div>
+                </div>
+                <div class="card mb-2 mr-2" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="eagle">
+                    <div class="card-body">
+                        <h3 class="card-text">Eagle</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="card mb-2 mr-2" style="width: 18rem;">
+                    <img src="dolphin.png" class="card-img-top" alt="hornbill">
+                    <div class="card-body">
+                        <h3 class="card-text">Hornbill</h3>
+                    </div>
+                </div>
+                <div class="card mb-2 mr-2" style="width: 18rem;">
+                    <img src="img/dove.png" class="card-img-top" alt="komodo">
+                    <div class="card-body">
+                        <h3 class="card-text">Dove</h3>
+                    </div>
+                </div>
+                <div class="card mb-2 mr-2" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="lion">
+                    <div class="card-body">
+                        <h3 class="card-text">Lion</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="card mb-2 mr-2" style="width: 18rem;">
+                    <img src="dolphin.png" class="card-img-top" alt="mantaray">
+                    <div class="card-body">
+                        <h3 class="card-text">Mantaray</h3>
+                    </div>
+                </div>
+                <div class="card mb-2 mr-2" style="width: 18rem;">
+                    <img src="img/dove.png" class="card-img-top" alt="rhino">
+                    <div class="card-body">
+                        <h3 class="card-text">Rhino</h3>
+                    </div>
+                </div>
+                <div class="card mb-2 mr-2" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="shark">
+                    <div class="card-body">
+                        <h3 class="card-text">Shark</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
