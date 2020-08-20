70ad33256e8befcafd5de67f6ebceb8a047329a4
mgstabrani <mgstabrani19@gmail.com>
2020-08-21 00:18:46 +0700

    Creating download feature and fixing images display

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,146 +3,81 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
         <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-            <div class="container mt-5 ">
-            <div class="row">
+        <div class="container mt-5 ">
+            <div class="row justify-content-center">
                 <div class="card mb-2 mr-2" style="width: 18rem;">
-                    <img src="dolphin.png" class="card-img-top" alt="dolphin">
+                    <img src="img/dolphin.png" class="card-img-top" alt="dolphin" width="128" height="128">
                     <div class="card-body">
+                        <a href="/pdf/dolphin.pdf" download>
                         <h3 class="card-text">Dolphin</h3>
+                        </a>
                     </div>
                 </div>
                 <div class="card mb-2 mr-2" style="width: 18rem;">
-                    <img src="img/dove.png" class="card-img-top" alt="dove">
                     <div class="card-body">
+                        <img src="img/dove.png" class="card-img-top" alt="dove" width="128" height="128">
+                        <a href="/pdf/dove.pdf" download>
                         <h3 class="card-text">Dove</h3>
                     </div>
                 </div>
                 <div class="card mb-2 mr-2" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="eagle">
+                    <img src="img/eagle.jpg" class="card-img-top" alt="eagle" width="128" height="128">
                     <div class="card-body">
+                        <a href="/pdf/eagle.pdf" download>
                         <h3 class="card-text">Eagle</h3>
                     </div>
                 </div>
             </div>
-            <div class="row">
+            <div class="row justify-content-center">
                 <div class="card mb-2 mr-2" style="width: 18rem;">
-                    <img src="dolphin.png" class="card-img-top" alt="hornbill">
+                    <img src="img/hornbill.png" class="card-img-top" alt="dove" width="128" height="128">
                     <div class="card-body">
+                        <a href="/pdf/hornbill.pdf" download>
                         <h3 class="card-text">Hornbill</h3>
                     </div>
                 </div>
                 <div class="card mb-2 mr-2" style="width: 18rem;">
-                    <img src="img/dove.png" class="card-img-top" alt="komodo">
+                    <img src="img/komodo.png" class="card-img-top" alt="komodo" width="128" height="128">
                     <div class="card-body">
-                        <h3 class="card-text">Dove</h3>
+                        <a href="/pdf/komodo.pdf" download>
+                        <h3 class="card-text">Komodo</h3>
                     </div>
                 </div>
                 <div class="card mb-2 mr-2" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="lion">
+                    <img src="img/lion.png" class="card-img-top" alt="dove" width="128" height="128">
                     <div class="card-body">
+                        <a href="/pdf/lion.pdf" download>
                         <h3 class="card-text">Lion</h3>
                     </div>
                 </div>
             </div>
-            <div class="row">
+            <div class="row justify-content-center">
                 <div class="card mb-2 mr-2" style="width: 18rem;">
-                    <img src="dolphin.png" class="card-img-top" alt="mantaray">
+                    <img src="img/mantaray.png" class="card-img-top" alt="mantaray" width="128" height="128">
                     <div class="card-body">
+                        <a href="/pdf/mantaray.pdf" download>
                         <h3 class="card-text">Mantaray</h3>
                     </div>
                 </div>
                 <div class="card mb-2 mr-2" style="width: 18rem;">
-                    <img src="img/dove.png" class="card-img-top" alt="rhino">
+                    <img src="img/rhino.png" class="card-img-top" alt="rhino" width="128" height="128">
                     <div class="card-body">
+                        <a href="/pdf/rhino.pdf" download>
                         <h3 class="card-text">Rhino</h3>
                     </div>
                 </div>
                 <div class="card mb-2 mr-2" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="shark">
+                    <img src="img/shark.jpg" class="card-img-top" alt="shark" width="128" height="128">
                     <div class="card-body">
+                        <a href="/pdf/shark.pdf" download>
                         <h3 class="card-text">Shark</h3>
                     </div>
                 </div>
-            </div>
-        </div>
-                
             </div>
         </div>
     </body>
